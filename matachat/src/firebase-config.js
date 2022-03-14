704d8d14c9b09510e519ab2025e22c553f887c29
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-13 17:50:18 -0700

    Create firebase-config.js

---
+++ matachat/src/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "------------INSERT HERE FROM ENV FILE--------------------------",
+  authDomain: "matachat-7ecf2.firebaseapp.com",
+  projectId: "matachat-7ecf2",
+  storageBucket: "matachat-7ecf2.appspot.com",
+  messagingSenderId: "658686286791",
+  appId: "1:658686286791:web:e14f3670a7b6bf825a17e2"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
