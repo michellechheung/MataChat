7f084dc6eb9e5448587cefbf2b15e5f270c83cb4
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-13 18:11:48 -0700

    Update App.js

--- matachat/src/App.js
+++ matachat/src/App.js
@@ -1,1 +1,12 @@
 
+import {useState, useEffect } from "react";
+import {db} from "./firebase-config";
+import {collection, getDocs} from "firebase/firestore";
+
+function App(){
+  
+  
+  return (
+    <h1> Hello World </h1>
+  );
+}
