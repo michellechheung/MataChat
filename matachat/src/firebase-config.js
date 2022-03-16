b26a7680ecbaf66d02e795aa81603e22246ca7c9
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-16 01:57:29 -0700

    fully function register for only CSN students

--- matachat/src/firebase-config.js
+++ matachat/src/firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app"; 
-import { getAuth} from "firebase/auth"
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
 import {getFirestore} from "@firebase/firestore";
 import config from "./config";
 var key = config.API_KEY; // pulls API key from src/congig.js
@@ -15,3 +15,51 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+const registerUser = async (name, email, password) => {
+  try {
+    if(email.trim().endsWith("csun.edu")){
+      const response = await createUserWithEmailAndPassword(auth, email.trim(), password);
+      const user = response.user;
+      console.log(user);
+    }
+    else{
+      throw new Error ("The provided value for the email user property is invalid. It must be a valid CSUN email address", "auth/invalid-csun");
+    }
+    
+    
+
+  } catch (error) {
+    console.error("new error" + error + " code: " + error.code);
+    
+    let message;
+
+    switch(error.code){
+      case "auth/email-already-in-use":
+        message = "The provided email is already in use by an existing user. Each user must have a unique email";
+        break;
+      case "auth/invalid-email":
+        message= "The provided value for the email property is invalid. It must be a valid email address";
+        break;
+      case "auth/weak-password":
+        message="Password should be at least 6 characters"
+        break;
+      case "auth/invalid-csun":
+        message="The provided value for the email property is invalid. It must be a valid CSUN email address"
+      default:
+        message= error.message;
+    }
+    alert(message);
+  }
+};
+
+
+//TEST FUNCTION
+var name = "kyle";
+var email = "testFromReact@ucla.edu";
+var password = "passssss"; 
+registerUser(name, email, password);
+
+  
+
+
